2375112e7db3d345745c56ecf0b350bada7b6404
vladsav1980 <vladsav1980@gmail.com>
2023-08-16 09:24:53 +0300

    accessability added

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
 
-function BookingForm({ availableTimes, updateTimes }) {
+function BookingForm({ availableTimes, updateTimes, submitForm }) {
   const [date, setDate] = useState("");
   const [time, setTime] = useState("17:00");
   const [numGuests, setNumGuests] = useState("");
   const [occasion, setOccasion] = useState("");
-  
+
   useEffect(() => {
     // Оновлюємо часи доступних бронювань на основі поточної дати
     updateTimes(date);
@@ -15,14 +15,44 @@ function BookingForm({ availableTimes, updateTimes }) {
     setDate(e.target.value);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Зупиняємо стандартну дію відправки форми
+
+    // Перевіряємо на коректність введених даних
+    if (date === "" || time === "" || numGuests <= 0 || occasion === "") {
+      alert("Please fill in all fields correctly.");
+      return;
+    }
+
+    // Викликаємо функцію для відправки форми
+    submitForm({
+      date: date,
+      time: time,
+      numGuests: numGuests,
+      occasion: occasion,
+    });
+
+    // Очищаємо поля форми після відправки
+    setDate("");
+    setTime("17:00");
+    setNumGuests("");
+    setOccasion("");
+  };
+
   return (
-    <form style={{ display: "grid", maxWidth: "200px", gap: "20px" }}>
+    <form
+      style={{ display: "grid", maxWidth: "200px", gap: "20px" }}
+      onSubmit={handleSubmit}
+      aria-label="Booking Form"
+    >
       <label htmlFor="res-date">Choose date</label>
       <input
         type="date"
         id="res-date"
         value={date}
         onChange={handleDateChange}
+        required
+        aria-label="Select a date"
       />
 
       <label htmlFor="res-time">Choose time</label>
@@ -30,6 +60,8 @@ function BookingForm({ availableTimes, updateTimes }) {
         id="res-time"
         value={time}
         onChange={(e) => setTime(e.target.value)}
+        required
+        aria-label="Select a time"
       >
         {availableTimes.map((time) => (
           <option key={time} value={time}>
@@ -44,6 +76,9 @@ function BookingForm({ availableTimes, updateTimes }) {
         id="num-guests"
         value={numGuests}
         onChange={(e) => setNumGuests(e.target.value)}
+        min="1"
+        required
+        aria-label="Enter the number of guests"
       />
 
       <label htmlFor="occasion">Occasion</label>
@@ -52,9 +87,13 @@ function BookingForm({ availableTimes, updateTimes }) {
         id="occasion"
         value={occasion}
         onChange={(e) => setOccasion(e.target.value)}
+        required
+        aria-label="Enter the occasion"
       />
 
-      <button type="submit">Submit reservation</button>
+      <button type="submit" aria-label="Submit reservation">
+        Submit reservation
+      </button>
     </form>
   );
 }
